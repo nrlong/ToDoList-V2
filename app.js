323786b80dfa0bf4ca8f33cf409878029e005d0c
Nathan Long <nrlong13@gmail.com>
2021-03-01 16:54:55 -0500

    initial commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'))
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://admin:AccessToDB@todolist.4lrle.mongodb.net/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true})
 
 const itemsSchema = new mongoose.Schema ({
     name:{
